5c53cd4bcd8d6ca40d62183ed2dc4302983760e6
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-09-09 16:51:04 -0300

    add routing table functions

---
+++ aws/route.sh
@@ -0,0 +1,41 @@
+# Routing table specific functions
+
+fn aws_routetbl_create(vpcid, tags) {
+	tblid <= (
+		aws ec2 create-route-table --vpc-id $vpcid |
+                jq ".RouteTable.RouteTableId"
+	)
+
+	aws_tag($tblid, $tags)
+        return $tblid
+}
+
+fn aws_route2igw(tblid, cidr, igwid) {
+	(aws ec2 create-route	--route-table-id $tblid
+				--destination-cidr-block $cidr
+				--gateway-id $igwid)
+}
+
+fn aws_route2vpcpeer(tblid, cidr, peerid) {
+	(aws ec2 create-route	--route-table-id $tblid
+				--destination-cidr-block $cidr
+				--vpc-peering-connection-id $peerid)
+}
+
+fn aws_route2nat(tblid, cidr, natid) {
+	(aws ec2 create-route	--route-table-id $tblid
+				--destination-cidr-block $cidr
+				--nat-gateway-id $natid)
+}
+
+fn aws_route2netif(tblid, cidr, netifid) {
+	(aws ec2 create-route	--route-table-id $tblid
+				--destination-cidr-block $cidr
+				--network-interface-id $netifid)
+}
+
+fn aws_route2instance(tblid, cidr, instid) {
+	(aws ec2 create-route	--route-table-id $tblid
+				--destination-cidr-block $cidr
+				--instance-id $instid)
+}
