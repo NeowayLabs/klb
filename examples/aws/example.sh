ac6343b924179a424e3cdeaac4569615e8d4bde9
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-09-09 21:57:25 -0300

    fix bugs, typos and add sg

--- examples/aws/example.sh
+++ examples/aws/example.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env nash
 
+import nashlib/all
+import klb/aws/all
+
 vpcTags = (
 	(Name klb-vpc-example)
 	(Env testing)
@@ -27,16 +30,18 @@ dbSubnetTags = (
 
 fn create_prod() {
 	vpcid	<= aws_vpc_create("10.0.0.1/16", $vpcTags)
-	appnet	<= aws_subnet_create("10.0.1.0/24", $appSubnetTags)
-	dbnet	<= aws_subnet_create("10.0.2.0/24", $dbSubnetTags)
+	appnet	<= aws_subnet_create($vpcid, "10.0.1.0/24", $appSubnetTags)
+	dbnet	<= aws_subnet_create($vpcid, "10.0.2.0/24", $dbSubnetTags)
 	igwid	<= aws_igw_create($igwTags)
 	tblid	<= aws_routetbl_create($vpcid, $routeTblTags)
 
 	aws_igw_attach($igwid, $vpcid)
-        aws_route2igw(tblid, "0.0.0.0/0", $igwid)
+        aws_route2igw($tblid, "0.0.0.0/0", $igwid)
 
+	grpid <= aws_secgroup_create("klb-default-sg", "Default	security group", $vpcid)
 
-	# TODO
+        printf "Created VPC: %s%s%s" $NASH_GREEN $vpcid $NASH_RESET
+	printf "Security group %s created\n" $grpid
 }
 
 create_prod()
