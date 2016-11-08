f3b9bfcb30a648eeed4e7f2fb8e1d29907049099
Matheus Vill <matheusvill@gmail.com>
2016-11-08 11:33:58 -0200

    Add method to create and delete subnet

---
+++ azure/subnet.sh
@@ -0,0 +1,17 @@
+# Subnet related functions
+
+fn azure_subnet_create(name, group, vnet, address, securitygroup) {
+	azure network vnet subnet create 
+		--name $name
+		--resource-group $group
+		--vnet-name $vnet
+		--address-prefixes $address
+		--network-security-group-name $securitygroup        
+}
+
+fn azure_subnet_delete(name, group, vnet) {
+	azure network vnet subnet delete 
+		--resource-group $group
+		--vnet-name $vnet
+		--name $name
+}
