8c0ea8401da97fadea45c1104d3befe6371af0c4
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-11-11 13:36:26 -0200

    Use nash API to set variables instead of contatenate strings
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

--- tests/azure/resourcegroup_test.go
+++ tests/azure/resourcegroup_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/NeowayLabs/klb/tests/azure"
 	"github.com/NeowayLabs/klb/tests/nash"
+	"github.com/NeowayLabs/nash/sh"
 )
 
 var (
@@ -17,12 +18,29 @@ var (
 func TestHandleResourceGroupLifeCycle(t *testing.T) {
 	session := azure.NewSession(t)
 
-	nash.Exec(t, "TestHandleResourceGroupLifeCycle", `
-             azure login -q -u `+session.ClientID+` --service-principal --tenant `+session.TenantID+` -p `+session.ClientSecret+`
+	shell := nash.Setup(t)
+	shell.Setvar("ResourceGroup", sh.NewStrObj(ResourceGroupName))
 
+	shell.Setvar("AZURE_CLIENT_ID", sh.NewStrObj(session.ClientID))
+	shell.Setvar("AZURE_TENANT_ID", sh.NewStrObj(session.TenantID))
+	shell.Setvar("AZURE_CLIENT_SECRET", sh.NewStrObj(session.ClientSecret))
+
+	err := shell.Exec("setup-login", `
+	azure login -q -u $AZURE_CLIENT_ID --service-principal --tenant $AZURE_TENANT_ID -p $AZURE_CLIENT_SECRET
+	`)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = shell.Exec("TestHandleResourceGroupLifeCycle", `
              import ../../azure/all
-             azure_group_create("`+ResourceGroupName+`", "eastus")
+             azure_group_create($ResourceGroup, "eastus")
         `)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	resources := azure.NewResources(t, session)
 	defer resources.Delete(t, ResourceGroupName)
