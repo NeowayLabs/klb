f68989b7dd00d7115007c7e3f32c8b38401723f7
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2016-11-16 17:47:21 -0200

    Added vnet_set_route_table (#12)

--- azure/vnet.sh
+++ azure/vnet.sh
@@ -7,3 +7,7 @@ fn azure_vnet_create(name, group, location, cidr) {
 fn azure_vnet_delete(name, group) {
 	azure network vnet delete --resource-group $group --name $name
 }
+
+fn azure_vnet_set_route_table(name, group, vnet, subnet, routetable) {
+	azure network vnet subnet set --name $name --resource-group $group --vnet-name $vnet --route-table-name $routetable
+}
