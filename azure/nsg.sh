e48a0dc7941348deab4158f464f87b61d1aed193
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-17 20:23:03 -0200

    Refactoring network security group

--- azure/nsg.sh
+++ azure/nsg.sh
@@ -2,56 +2,96 @@
 
 fn azure_nsg_create(name, group, location) {
 	(
-		azure network nsg create
-			--name $name
-			--resource-group $group
-			--location $location
+		azure network nsg create --name $name --resource-group $group --location $location
 	)
 }
 
 fn azure_nsg_delete(name, group) {
 	(
-		azure network nsg delete
-			--name $name
-			--resource-group $group
+		azure network nsg delete --name $name --resource-group $group
 	)
 }
 
-fn azure_nsg_add_inbound_rule(name, group, nsgname, priority, protocol, address, port, access) {
-	(
-		azure network nsg rule create
-		    --name $name
-		    --resource-group $group
-		    --nsg-name $nsgname
-		    --direction Inbound
-		    --priority $priority
-		    --protocol $protocol
-		    --source-address-prefix $address
-		    --destination-port-range $port
-		    --access $access
+fn azure_nsg_rule_new(name, group, nsgname, priority) {
+	instance = (
+		"--name"
+		$name
+		"--resource-group"
+		$group
+		"--nsg-name"
+		$nsgname
+		"--priority"
+		$priority
 	)
+
+	return $instance
 }
 
-fn azure_nsg_add_outbound_rule(name, group, nsgname, priority, protocol, address, port, access) {
-	(
-		azure network nsg rule create
-		    --name $name
-		    --resource-group $group
-		    --nsg-name $nsgname
-		    --direction Outbound
-		    --priority $priority
-		    --protocol $protocol
-		    --destination-address-prefix $address
-		    --destination-port-range $port
-		    --access $access
-	)
+fn azure_nsg_rule_set_description(instance, description) {
+	instance <= append($instance, "--description")
+	instance <= append($instance, $description)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_protocol(instance, protocol) {
+	instance <= append($instance, "--protocol")
+	instance <= append($instance, $protocol)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_source_address(instance, address) {
+	instance <= append($instance, "--source-address-prefix")
+	instance <= append($instance, $address)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_source_port(instance, port) {
+	instance <= append($instance, "--source-port-range")
+	instance <= append($instance, $port)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_destination_address(instance, address) {
+	instance <= append($instance, "--destination-address-prefix")
+	instance <= append($instance, $address)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_destination_port(instance, port) {
+	instance <= append($instance, "--destination-port-range")
+	instance <= append($instance, $port)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_access(instance, access) {
+	instance <= append($instance, "--access")
+	instance <= append($instance, $access)
+
+	return $instance
+}
+
+fn azure_nsg_rule_set_direction(instance, direction) {
+	instance <= append($instance, "--direction")
+	instance <= append($instance, $direction)
+
+	return $instance
+}
+
+fn azure_nsg_rule_create(instance) {
+	azure network nsg rule create $instance
 }
 
 fn azure_nsg_delete_rule(name, group, nsgname) {
 	(
 		azure network nsg rule delete
-		    --name $name
-		    --resource-group $group
-		    --nsg-name $nsgname
+					--name $name
+					--resource-group $group
+					--nsg-name $nsgname
 	)
 }
