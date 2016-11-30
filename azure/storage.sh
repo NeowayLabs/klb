7a37b61f0696ae51617af336d193c70fba7e7ab5
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 15:22:55 -0200

    Added doc azure_store_account_create function

--- azure/storage.sh
+++ azure/storage.sh
@@ -1,5 +1,15 @@
 # Storage related functions
 
+# azure_store_account_create creates a new `storage account`.
+# `name` is the storage account name
+# `group` is the resource group name
+# `location` is the azure region
+# `sku` is the SKU name (LRS/ZRS/GRS/RAGRS/PLRS)
+# `kind` is the account kind (Storage/BlobStorage)
+#
+# Following the Azure recommendation, this function add a random 3-digit hash
+# before your storage account name and return the `storage account` name created
+# Ref: https://docs.microsoft.com/en-us/azure/storage/storage-performance-checklist
 fn azure_storage_account_create(name, group, location, sku, kind) {
 	prefix <= head -n1 /dev/urandom | md5sum | cut -b 1-3
 
