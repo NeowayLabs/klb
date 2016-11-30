5cb585f039bbae756d448a45ff60359768f42e22
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 13:08:28 -0200

    Added return storage name

--- azure/storage.sh
+++ azure/storage.sh
@@ -3,9 +3,13 @@
 fn azure_storage_account_create(name, group, location, sku, kind) {
 	prefix <= head -n1 /dev/urandom | md5sum | cut -b 1-3
 
+	storagename = $prefix+$name
+
 	(
-		azure storage account create --resource-group $group --location $location --sku-name $sku --kind $kind $prefix+$name
+		azure storage account create --resource-group $group --location $location --sku-name $sku --kind $kind $storagename
 	)
+
+	return $storagename
 }
 
 fn azure_storage_account_delete(name, group) {
