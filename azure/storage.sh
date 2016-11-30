7cf46cfe6bf5539a0b91396f7306c03de5f559a1
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 11:31:13 -0200

    Added random prefix to storage account name

--- azure/storage.sh
+++ azure/storage.sh
@@ -1,13 +1,10 @@
 # Storage related functions
 
 fn azure_storage_account_create(name, group, location, sku, kind) {
+	prefix <= head -n1 /dev/urandom | md5sum | cut -b 1-3
+
 	(
-		azure storage account create
-			--resource-group $group
-			--location $location
-		    --sku-name $sku
-		    --kind $kind
-		    $name
+		azure storage account create --resource-group $group --location $location --sku-name $sku --kind $kind $prefix+$name
 	)
 }
 
