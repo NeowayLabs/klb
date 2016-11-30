14594cf61d7582e1b416e40eba07396c3d5dbb9f
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 16:38:28 -0200

    Added doc availset functions

--- azure/availset.sh
+++ azure/availset.sh
@@ -1,9 +1,16 @@
 # Availset related functions
 
+# azure_availset_create creates a new `availability set`.
+# `name` is the availability set name
+# `group` is the resource group name
+# `location` is the azure region
 fn azure_availset_create(name, group, location) {
 	azure availset create --name $name --resource-group $group --location $location
 }
 
+# azure_availset_delete deletes a exit `availability set`.
+# `name` is the availability set name
+# `group` is the resource group name
 fn azure_availset_delete(name, group) {
 	azure availset delete -q --name $name --resource-group $group
 }
