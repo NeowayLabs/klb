35beed43e057fb0d13573128defc4fa4ada65d9c
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 16:46:02 -0200

    Added doc azure ad app functions

--- azure/app.sh
+++ azure/app.sh
@@ -1,3 +1,9 @@
+# Active Directory applications related functions
+
+# azure_app_create creates a new `active directory application`.
+# `name` is the display name for the application
+# `homepage` is the URL to the application homepage
+# `uri` is the comma-delimitied URIs that identify the application
 fn azure_app_create(name, homepage, uri) {
 	out   <= (
 		azure ad app create --json
@@ -14,6 +20,8 @@ fn azure_app_create(name, homepage, uri) {
 	return $ret
 }
 
+# azure_app_delete deletes a exit `active directory application`.
+# `objid` is the display name for the application
 fn azure_app_delete(objid) {
 	azure ad app delete --objectid $objid
 }
