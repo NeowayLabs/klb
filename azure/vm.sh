b3c34a7f9accd38950c9de7bd70579c8cae9c473
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2016-11-30 23:36:02 -0200

    Added azure_vm_set_bootdiagnosticsstorageuri and fixed azure_vm_set_disablebootdiagnostic

--- azure/vm.sh
+++ azure/vm.sh
@@ -106,10 +106,21 @@ fn azure_vm_set_imageurn(instance, imageurn) {
 	return $instance
 }
 
-# azure_vm_set_disablebootdiagnostic
-# The switch parameter to disable the boot diagnostics (by default enabled) for VM.
-fn azure_vm_set_disablebootdiagnostics() {
+# azure_vm_set_disablebootdiagnostic switch parameter to disable
+# the boot diagnostics (by default enabled) for VM.
+# `instance` is the instance of vm.
+fn azure_vm_set_disablebootdiagnostics(instance) {
 	instance <= append($instance, "--disable-boot-diagnostics")
+
+	return $instance
+}
+
+# azure_vm_set_bootdiagnosticsstorageuri specify storage account for boot diagnostics.
+# `instance` is the instance of vm.
+# `storageaccount` is the name of storage account.
+fn azure_vm_set_bootdiagnosticsstorage(instance, storageaccount) {
+	instance <= append($instance, "--boot-diagnostics-storage-uri")
+	instance <= append($instance, "https://"+$storageuri+".blob.core.windows.net/")
 
 	return $instance
 }
