c4460a1cdc44330c8200a66ae27d2cc1585e514a
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-01-09 12:35:11 -0200

    Add retrier

---
+++ tests/lib/retrier/retrier.go
@@ -0,0 +1,71 @@
+//Package retrier implements a simple try again until
+//the given context gets cancelled. Useful to tests that can have
+//intermittent failures that do not represent a real failure,
+//avoiding false positives.
+package retrier
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type WorkFunc func() error
+
+// Run executes the given work function trying again if something
+// goes wrong. On success it just returns, if the context gets
+// cancelled it will call testing.T.Fatal with all the accumulated errors.
+func Run(
+	ctx context.Context,
+	t *testing.T,
+	work WorkFunc,
+) {
+	errs := retryUntilDone(ctx, work)
+
+	if len(errs) > 0 {
+		errmsgs := []string{
+			"\n",
+			"Test failed, errors in order:",
+		}
+
+		for i, err := range errs {
+			errmsgs = append(
+				errmsgs,
+				fmt.Sprintf("error[%d]: %s", i, err),
+			)
+		}
+
+		t.Fatal(t, strings.Join(errmsgs, "\n"))
+	}
+}
+
+func retryUntilDone(ctx context.Context, work WorkFunc) []error {
+	var errs []error
+	for {
+		// buffered channel avoid goroutine leak
+		result := make(chan error, 1)
+
+		go func() {
+			result <- work()
+		}()
+
+		select {
+		case res := <-result:
+			{
+				if res == nil {
+					return nil
+				}
+				errs = append(errs, res)
+			}
+		case <-ctx.Done():
+			{
+				return append(
+					errs,
+					errors.New("retrier timeout"),
+				)
+			}
+		}
+	}
+}
