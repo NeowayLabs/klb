51646c9028505eb1aeb4d52adb6a0731f7a7769c
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-01-09 17:52:07 -0200

    Adapt create test

--- tests/azure/resourcegroup_test.go
+++ tests/azure/resourcegroup_test.go
@@ -1,9 +1,13 @@
 package azure_test
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/NeowayLabs/klb/tests/lib/azure"
+	"github.com/NeowayLabs/klb/tests/lib/nash"
 )
 
 func genResourceGroupName() string {
@@ -11,34 +15,29 @@ func genResourceGroupName() string {
 }
 
 func testResourceGroupCreate(t *testing.T) {
-	//t.Parallel()
+	t.Parallel()
 
-	//ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	//defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
-	//resgroup := genResourceGroupName()
-	//session := azure.NewSession(t)
-	//resources := azure.NewResourceGroup(ctx, t, session)
-	//defer func() {
-	//ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	//defer cancel()
-	//resources := azure.NewResourceGroup(ctx, t, session)
-	//resources.Delete(t, resgroup)
-	//}()
+	resgroup := genResourceGroupName()
+	session := azure.NewSession(t)
+	resources := azure.NewResourceGroup(ctx, t, session)
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		resources := azure.NewResourceGroup(ctx, t, session)
+		resources.Delete(t, resgroup)
+	}()
 
-	//shell := nash.New(t)
-	//shell.Setvar("ResourceGroup", resgroup)
-
-	//err := shell.Exec("TestResourceGroupCreation", `
-	//import ../../azure/all
-	//azure_group_create($ResourceGroup, "eastus")
-	//`)
-
-	//if err != nil {
-	//t.Fatal(err)
-	//}
-
-	//resources.AssertExists(t, resgroup)
+	nash.Run(
+		ctx,
+		t,
+		"./testdata/create_resource_group.sh",
+		resgroup,
+		location,
+	)
+	resources.AssertExists(t, resgroup)
 }
 
 func testResourceGroupDelete(t *testing.T) {
