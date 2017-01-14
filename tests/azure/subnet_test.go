22acd849f78f213bb9e9de7f40837e19a84a113b
Kamila Hinckel <kamila.hinckel@neoway.com.br>
2017-01-14 15:15:00 -0200

    creating nsg and vnet to use in subnet

--- tests/azure/subnet_test.go
+++ tests/azure/subnet_test.go
@@ -14,13 +14,29 @@ func genSubnetName() string {
 }
 
 func testSubnetCreate(t *testing.T, f fixture.F) {
+	nsg := fmt.Sprintf("klb-nsg-tests-%d", rand.Intn(1000))
+	f.Shell.Run(
+		"./testdata/create_nsg.sh",
+		nsg,
+		f.ResGroupName,
+		f.Location,
+	)
+	vnet := genVnetName()
+	f.Shell.Run(
+		"./testdata/create_vnet.sh",
+		vnet,
+		f.ResGroupName,
+		f.Location,
+		"10.116.0.0/16",
+	)
 	subnet := genSubnetName()
 	f.Shell.Run(
 		"./testdata/create_subnet.sh",
 		subnet,
 		f.ResGroupName,
+		vnet,
 		f.Location,
-		"10.116.0.0/16",
+		nsg,
 	)
 	subnets := azure.NewSubnet(f)
 	subnets.AssertExists(t, subnet)
