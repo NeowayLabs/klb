5d17b30c33d3f64d628e727d16e5797a04660493
Kamila Hinckel <kahinckel@gmail.com>
2017-01-18 15:14:31 -0200

    add parameters to set in vm

--- tests/azure/vm_test.go
+++ tests/azure/vm_test.go
@@ -14,14 +14,92 @@ func genVMName() string {
 }
 
 func testVMCreate(t *testing.T, f fixture.F) {
+
 	vm := genVMName()
+	osType := "Linux"
+	vmSize := "Standard_DS4_v2"
+	username := "core"
+	availSet := genAvailSetName()
+	vnet := genVnetName()
+	subnet := genSubnetName()
+	nic := genNicName()
+	storAcc := genStorageAccountName()
+	osDisk := "test.vhd"
+	imageUrn := "OpenLogic:CentOS:7.2:7.2.20161026"
+	customData := "test.sh"
+	keyFile := "test.pub"
+
+	nsg := genNsgName()
+	vnetAddress := "10.116.0.0/16"
+	subnetAddress := "10.116.1.0/24"
+	addrnic := "10.116.1.100"
+
+	f.Shell.Run(
+		"./testdata/create_avail_set.sh",
+		f.ResGroupName,
+		availSet,
+		f.Location,
+	)
+
+	f.Shell.Run(
+		"./testdata/create_vnet.sh",
+		vnet,
+		f.ResGroupName,
+		f.Location,
+		vnetAddress,
+	)
+
+	f.Shell.Run(
+		"./testdata/create_nsg.sh",
+		nsg,
+		f.ResGroupName,
+		f.Location,
+	)
+
+	f.Shell.Run(
+		"./testdata/create_subnet.sh",
+		subnet,
+		f.ResGroupName,
+		vnet,
+		subnetAddress,
+		nsg,
+	)
+
+	f.Shell.Run(
+		"./testdata/create_nic.sh",
+		f.ResGroupName,
+		nic,
+		f.Location,
+		vnet,
+		subnet,
+		addrnic,
+	)
+
+	f.Shell.Run(
+		"./testdata/create_storage_account.sh",
+		f.ResGroupName,
+		storAcc,
+		f.Location,
+	)
+	//storage := os.Getenv("STORAGE_ACCOUNT_NAME")
 
 	f.Shell.Run(
 		"./testdata/create_vm.sh",
 		vm,
 		f.ResGroupName,
 		f.Location,
-		"Linux",
+		osType,
+		vmSize,
+		username,
+		availSet,
+		vnet,
+		subnet,
+		nic,
+		storAcc,
+		osDisk,
+		imageUrn,
+		customData,
+		keyFile,
 	)
 	vms := azure.NewVM(f)
 	vms.AssertExists(t, vm)
