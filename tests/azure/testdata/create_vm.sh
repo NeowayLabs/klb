92cfe5d662ce534d49109e95994c75d5da6fe070
Kamila Hinckel <kahinckel@gmail.com>
2017-01-18 11:59:12 -0200

    add script to create vm

---
+++ tests/azure/testdata/create_vm.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env nash
+
+import ../../azure/login
+import ../../azure/nsg
+
+name     = $ARGS[1]
+resgroup = $ARGS[2]
+location = $ARGS[3]
+ostype   = $ARGS[4]
+
+azure_login()
+azure_vm_new($name, $resgroup, $location, $ostype)
