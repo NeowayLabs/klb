e813fa98670d8159fba6cbfa979375487ed61dcb
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2017-03-13 12:26:42 -0300

    Improved createsp.sh tool

--- tools/azure/createsp.sh
+++ tools/azure/createsp.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env nash
 
-if len($ARGS) != 4 {
-        echo "usage: " + $ARGS[0] + " <subscription_id> <service_principal> <password>"
-        exit("0")
+if len($ARGS) != "4" {
+	echo "usage: "+$ARGS[0]+" <service_principal>  <subscription_id> <password>"
+	exit
 }
 
-subscription_id = $ARGS[1]
-service_principal = $ARGS[2]
-password = $ARGS[3]
+SERVICE_PRINCIPAL = $ARGS[1]
+SUBSCRIPTION_ID   = $ARGS[2]
+PASSWORD          = $ARGS[3]
 
-azure account set $subscription_id
-azure ad sp create -n $service_principal -p $password
+SUBSCRIPTION_NAME <= azure account show $SUBSCRIPTION_ID --json | jq -r ".[0].name"
+
+echo "Setting azure subscription to: "+$SUBSCRIPTION_NAME+" ["+$SUBSCRIPTION_ID+"]"
+azure account set $SUBSCRIPTION_ID >[1=]
+echo "Creating azure service principal: "+$SERVICE_PRINCIPAL
+
+SERVICE_PRINCIPAL_OBJECT_ID <= azure ad sp create -n $SERVICE_PRINCIPAL -p $PASSWORD --json | jq -r ".objectId"
+
+echo "Granting access to "+$SERVICE_PRINCIPAL+" ["+$SERVICE_PRINCIPAL_OBJECT_ID+"] at: "+$SUBSCRIPTION_NAME+" ["+$SUBSCRIPTION_ID+"]"
+sleep 5
+azure role assignment create --objectId $SERVICE_PRINCIPAL_OBJECT_ID -o Owner -c "/subscriptions/"+$SUBSCRIPTION_ID+"/" >[1=]
+echo "Done"
