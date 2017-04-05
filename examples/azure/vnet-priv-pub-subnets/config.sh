16d4384760575f93024b1a3e8dc5fc0a0f54da8c
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2017-04-05 10:28:03 -0300

    Fixed vm configs

--- examples/azure/vnet-priv-pub-subnets/config.sh
+++ examples/azure/vnet-priv-pub-subnets/config.sh
@@ -8,6 +8,7 @@ location = "eastus"
 vnet             = "vnet"
 vnet_cidr        = "10.50.0.0/16"
 vnet_dns_servers = ("8.8.8.8" "8.8.4.4")
+
 subnet_pub_name  = "public"
 subnet_pub_cidr  = "10.50.1.0/24"
 subnet_priv_name = "private"
@@ -17,9 +18,17 @@ subnet_priv_cidr = "10.50.2.0/24"
 
 vm_size         = "Basic_A2"
 vm_username     = "core"
-vm_image        = "CoreOS:CoreOS:Stable:1298.6.0"
-nat_name        = "vm-nat"
+vm_image_urn    = "CoreOS:CoreOS:Stable:1298.6.0"
+vm_storage_type = "LRS"
+
+nat_name        = "nat"
+nat_subnet      = "public"
 nat_address     = "10.50.1.100"
-bastion_name    = "vm-bastion"
+
+bastion_name    = "bastion"
+bastion_subnet  = "public"
 bastion_address = "10.50.1.200"
-app_name        = "vm-app"
+
+app_name        = "app"
+app_subnet      = "private"
+app_address     = "10.50.2.10"
