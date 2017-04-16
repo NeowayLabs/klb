f18bf8867abad2729ff0480eb1c9980855c44e5a
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-16 18:49:31 -0300

    Try with sudo again

--- deps/debian.sh
+++ deps/debian.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o errexit
 echo "installing python pip and nodejs npm"
 apt-get install python-pip jq
+echo "installing nodejs"
+nvm install 7.7.4
 echo "installing azure cli 1.0"
 npm install --no-optional -g azure-cli
 echo "installing azure cli 2.0"
