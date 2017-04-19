0ff240affdfece57f24a794adebc5b4d905011d6
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-04-19 11:40:38 -0300

    Fix run-ci

--- hack/run-ci.sh
+++ hack/run-ci.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 source ./hack/loadenv.sh
-docker run -v `pwd`: $WORKDIR -w $WORKDIR --env-file $DOCKER_ENV neowaylabs/klb "$@"
+docker run -v `pwd`:$WORKDIR -w $WORKDIR --env-file $DOCKER_ENV neowaylabs/klb "$@"
