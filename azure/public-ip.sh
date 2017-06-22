94de7317757fb474331375965f1ccf17a117dbbd
Vitor Arins <vitorarins@gmail.com>
2017-06-22 16:03:48 -0300

    Add new function to get public ip address.

--- azure/public-ip.sh
+++ azure/public-ip.sh
@@ -6,6 +6,18 @@ fn azure_public_ip_create(name, group, location, allocation) {
 	)
 }
 
+fn azure_public_ip_get_address(name, group) {
+
+	public_ip_info    <= azure network public-ip show -g $group -n $name --json
+	if $public_ip_info == "{}" {
+	   print("no public ip found for resource group: '" + $group + "' and name: '" + $name + "'\n")
+	   return ""
+	}
+	public_ip_address <= echo $public_ip_info | jq -r ".ipAddress"
+
+	return $public_ip_address
+}
+
 fn azure_public_ip_delete(name, group) {
 	(
 		azure network public-ip delete --name $name --resource-group $group
