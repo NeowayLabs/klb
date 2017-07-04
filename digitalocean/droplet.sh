8ac0de9d37bab629819f103bac003caf1ca6e6c7
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2017-07-04 20:38:28 -0300

    Improved and add missing droplet configs

--- digitalocean/droplet.sh
+++ digitalocean/droplet.sh
@@ -14,33 +14,67 @@ fn digitalocean_droplet_create(name, size, image, region) {
 	return $instance
 }
 
-fn digitalocean_vm_set_backups(instance) {
+fn digitalocean_droplet_set_backups(instance) {
 	instance <= append($instance, "--enable-backups")
 
 	return $instance
 }
 
-fn digitalocean_vm_set_ipv6(instance) {
+fn digitalocean_droplet_set_ipv6(instance) {
 	instance <= append($instance, "--enable-ipv6")
 
 	return $instance
 }
 
-fn digitalocean_vm_set_monitoring(instance) {
+fn digitalocean_droplet_set_monitoring(instance) {
 	instance <= append($instance, "--enable-monitoring")
 
 	return $instance
 }
 
-fn digitalocean_vm_set_private_network(instance) {
+fn digitalocean_droplet_set_private_network(instance) {
 	instance <= append($instance, "--enable-private-networking")
 
 	return $instance
 }
 
-fn digitalocean_vm_set_ssh_key(instance, keys) {
+fn digitalocean_droplet_set_ssh_key(instance, keys) {
 	instance <= append($instance, "--ssh-keys")
 	instance <= append($instance, $keys)
+
+	return $instance
+}
+
+fn digitalocean_droplet_set_user_data_file(instance, file) {
+	instance <= append($instance, "--user-data-file")
+	instance <= append($instance, $file)
+
+	return $instance
+}
+
+fn digitalocean_droplet_set_tag_name(instance, tag) {
+	instance <= append($instance, "--tag-name")
+	instance <= append($instance, $tag)
+
+	return $instance
+}
+
+fn digitalocean_droplet_set_tag_names(instance, tags) {
+	instance <= append($instance, "--tag-names")
+	instance <= append($instance, $tags)
+
+	return $instance
+}
+
+fn digitalocean_droplet_set_tag_volumes(instance, volumes) {
+	instance <= append($instance, "--volumes")
+	instance <= append($instance, $volumes)
+
+	return $instance
+}
+
+fn digitalocean_droplet_set_wait(instance) {
+	instance <= append($instance, "--wait")
 
 	return $instance
 }
@@ -48,10 +82,3 @@ fn digitalocean_vm_set_ssh_key(instance, keys) {
 fn digitalocean_droplet_create(instance) {
 	doctl compute droplet create $instance
 }
-
-# TODO
-#      --tag-name string             Tag name
-#      --tag-names value             Tag names (default [])
-#      --user-data-file string       User data file
-#      --volumes value               Volumes to attach (default [])
-#      --wait                        Wait for droplet to be created
