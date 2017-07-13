bdc6bb613ec975a5eef5fbe891b43f4e2e34aa80
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-07-13 12:05:41 -0300

    Add error as a return on azure_vm_backup_delete

--- examples/azure/backup/config.sh
+++ examples/azure/backup/config.sh
@@ -17,7 +17,7 @@ vm_size         = "Standard_DS15_v2"
 vm_username     = "core"
 vm_image_urn    = "CoreOS:CoreOS:Stable:1298.6.0"
 vm_disks_count  = "4"
-vm_disks_size   = "1023"
+vm_disks_size   = "50"
 
 backup_prefix   = "klb-ex"
 backup_location = "eastus2"
--- tests/azure/testdata/delete_backups.sh
+++ tests/azure/testdata/delete_backups.sh
@@ -16,7 +16,12 @@ backups <= azure_vm_backup_list($vmname, $bkpprefix)
 for backup in $backups {
 	echo "deleting backup: "+$backup
 
-	azure_vm_backup_delete($backup)
+	err <= azure_vm_backup_delete($backup)
+	if $err != "" {
+		echo "unable to delete backup: " + $backup
+		echo "error: " + $err
+		exit("1")
+	}
 
 	echo "deleted backup: "+$backup
 }
