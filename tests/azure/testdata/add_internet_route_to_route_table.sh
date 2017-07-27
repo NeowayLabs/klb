b022765511233b485cec5772a8e7ff248a40d6bc
Luciano Antonio Borguetti Faustino <luciano.faustino@neoway.com.br>
2017-07-27 14:06:10 -0300

    Fixed typo

--- tests/azure/testdata/add_internet_route_to_route_table.sh
+++ tests/azure/testdata/add_internet_route_to_route_table.sh
@@ -10,13 +10,13 @@ hoptype    = $ARGS[5]
 
 azure_login()
 
-fn get_route_table_id() {
-	routetableid, status <= azure_route_table_get_id($routetable, $resgroup)
+fn get_route_id() {
+	routetableid, status <= azure_route_table_route_get_id($name, $resgroup, $routetable)
 
 	return $routetableid
 }
 
-route_id <= get_route_table_id()
+route_id <= get_route_id()
 
 if $route_id == "" {
 	route <= azure_route_table_route_new($name, $resgroup, $routetable, $address, $hoptype)
@@ -24,7 +24,7 @@ if $route_id == "" {
 	azure_route_table_route_create($route)
 }
 
-route_id <= get_route_table_id()
+route_id <= get_route_id()
 
 if $route_id == "" {
 	exit("1")
